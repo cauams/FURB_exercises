023068c86edd30f6f34410670ebf9695cdbb0d8b
cauams <cauanzitobnu@gmail.com>
2022-05-16 19:24:10 -0300

    Uni5Exe08 done

--- AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe08.java
+++ AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe08.java
@@ -11,8 +11,25 @@ public class Uni5Exe08 {
         List<Integer> numberlist = new ArrayList<Integer>();
 
         System.out.println("Insira a quantidade de números");
+        int num = scanner.nextInt();
+        double numerosPositivos = 0;
+        int qntNumerosPositivos = 0;
         
-
+        for (int i = 1; i <= num; i++) {
+        	System.out.println("Digite números! " + i + " de " + num);
+        	int numero = scanner.nextInt();
+        	
+        	if(numero < 0) {
+        		numberlist.add(numero);
+        	} else {
+        		qntNumerosPositivos++;
+        		numerosPositivos += numero;      		
+        	}
+        }
+        
+        System.out.println("O menor número negativo é: " + getMin(numberlist));
+        System.out.println("A média dos números positivos é de: " +  numerosPositivos / qntNumerosPositivos);
+        
 
         scanner.close();
     }
