8f4fea28835254484476b2a74fc308df1a3bb9f4
GustavoSiebertEmmendoerfer <86327093+GustavoSiebertEmmendoerfer@users.noreply.github.com>
2022-05-16 20:27:42 -0300

    Uni5Exe12 done

---
+++ AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe12.java
@@ -0,0 +1,21 @@
+package unidade05.exerciciosSala;
+
+import java.util.Scanner;
+
+public class Uni5Exe12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escreva numero de vezes que a piramide");
+        int nmbVezes = sc.nextInt();
+        int result = 1;
+        for(int i = 1; i <= nmbVezes; i++)
+        {
+            for(int ii = 1; ii <= i; ii++)       
+            {
+                System.out.print(result + " ");
+                result += 1;
+            }
+            System.out.println();
+        }
+    }
+}
