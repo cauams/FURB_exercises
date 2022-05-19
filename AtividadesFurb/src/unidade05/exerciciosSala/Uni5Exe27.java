acb8e92de222174d23ba7a4f45d29083837bdc4c
Cauã <cauanzitobnu@gmail.com>
2022-05-19 19:17:24 -0300

    Uni5Exe27 done

--- AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe27.java
+++ AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe27.java
@@ -2,17 +2,15 @@ package unidade05.exerciciosSala;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Comparator;
 
 public class Uni5Exe27 {
 
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
     DecimalFormat df = new DecimalFormat("0.00");
-
 
     List<Integer> producao = new ArrayList<Integer>();
     List<Integer> dias = new ArrayList<Integer>();
@@ -27,7 +25,6 @@ public class Uni5Exe27 {
       if (dia < 0 || dia > 31) {
         System.out.println("Dia inválido!");
       } else {
-
         dias.add(dia);
 
         System.out.println("Digite a producao do periodo matutino: ");
@@ -38,21 +35,29 @@ public class Uni5Exe27 {
         producaoTotal = (producaoManha + producaoTarde);
         producao.add(producaoTotal);
 
-        if(dia <= 15 && producaoManha >= 30 && producaoTarde >=30) {
-          salario = producaoTotal * 0.8; 
+        if (dia <= 15 && producaoManha >= 30 && producaoTarde >= 30) {
+          salario = producaoTotal * 0.8;
         }
-        if(dia <= 15) {
+        if (dia <= 15) {
           salario = producaoTotal * 0.5;
         }
-        if(dia >= 16) {
+        if (dia >= 16) {
           salario = (producaoManha * 0.4) + (producaoTarde * 0.3);
         }
-        
-        if(producaoManha > producaoTarde) {
-          System.out.println("A produção da manhã foi maior que a da tarde; " + producaoManha + " produtos produzidos." );
+
+        if (producaoManha > producaoTarde) {
+          System.out.println(
+            "A produção da manhã foi maior que a da tarde; " +
+            producaoManha +
+            " produtos produzidos."
+          );
         }
-        if(producaoTarde > producaoManha) {
-          System.out.println("A produção da tarde foi maior que a produção da manhã; " + producaoTarde + " produtos produzidos.");
+        if (producaoTarde > producaoManha) {
+          System.out.println(
+            "A produção da tarde foi maior que a produção da manhã; " +
+            producaoTarde +
+            " produtos produzidos."
+          );
         }
 
         System.out.println("R$ " + df.format(salario) + " (valor recebido)");
@@ -61,21 +66,27 @@ public class Uni5Exe27 {
         System.out.println("Novo funcionário? (1 - sim, 2 - não)");
         int opt = scanner.nextInt();
 
-        if(opt == 1) {
+        if (opt == 1) {
           System.out.println("////////////////////");
         }
-        if(opt == 2) {
-          System.out.println("Resultados de desempenho obtidos e sistema finalizado.");
+        if (opt == 2) {
+          System.out.println(
+            "Resultados de desempenho obtidos e sistema finalizado."
+          );
           break;
         }
-        
       }
-      
     }
 
-    int maxIndex = producao.indexOf(producao.stream().max(Comparator.naturalOrder()).get());
+    int maxIndex = producao.indexOf(
+      producao.stream().max(Comparator.naturalOrder()).get()
+    );
 
-    System.out.println("O dia onde obteve-se a maior produção foi o dia: " + dias.get(maxIndex) + " de Abril.");
+    System.out.println(
+      "O dia onde obteve-se a maior produção foi o dia: " +
+      dias.get(maxIndex) +
+      " de Abril."
+    );
 
     scanner.close();
   }
