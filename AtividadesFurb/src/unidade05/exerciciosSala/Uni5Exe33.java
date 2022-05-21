596c451a51b13e336a9996253cb05cdb49ad5fc7
Cauã <cauanzitobnu@gmail.com>
2022-05-21 00:04:12 -0300

    HOT FIX

--- AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe33.java
+++ AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe33.java
@@ -5,21 +5,6 @@ import java.util.Scanner;
 public class Uni5Exe33 {
 
   public static void main(String[] args) {
-    /*
-        Em uma eleição para presidência, existem 4 candidatos. Os votos são informados através de código. Os dados utilizados para votação obedecem a seguinte codificação:
-
-1, 2, 3, 4 = voto para o respectivo candidato;
-5 = voto nulo;
-6 = voto em branco.
-Elabore um programa que calcule e escreva:
-
-total de votos para cada candidato;
-total de votos nulos;
-total de votos em branco;
-percentual dos votos em branco e nulos sobre o total.
-Se o usuário informar um número de operação incorreto, emitir a mensagem “Opção incorreta” e persistir solicitando um número de opção correto. Para interromper a operação, o usuário poderá fornecer o número 0.
-        
-        */
 
     int candidato1 = 0;
     int candidato2 = 0;
--- AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe34.java
+++ AtividadesFurb/src/unidade05/exerciciosSala/Uni5Exe34.java
@@ -5,24 +5,6 @@ import java.util.Scanner;
 public class Uni5Exe34 {
 
     public static void main(String[] args) {
-        
-        /*
-        Um hotel cobra R$ 50,00 de diária por hóspede e mais uma taxa de serviços. A taxa de serviços é de:
-
-R$ 7,50 por diária, caso o número de diárias seja menor que 15;
-R$ 6,50 por diária, caso o número de diárias seja igual a 15;
-R$ 5,00 por diária, caso o número de diárias seja maior que 15.
-Descreva um algoritmo que apresente as seguintes opções ao recepcionista:
-
-(1) encerrar a conta de um hóspede;
-(2) verificar número de contas encerradas;
-(3) sair.
-Caso a opção escolhida seja a primeira, leia o nome e o número de diárias do hóspede e escreva o nome e total a ser pago. Caso a opção escolhida seja a segunda, informe o número de hóspedes que deixaram o hotel (número de contas encerradas). Caso a opção escolhida seja a terceira finalize a execução do algoritmo.
-
-
-        
-        
-        */
 
         String nome = "";
         double diarias = 0;
