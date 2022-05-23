b3f8cae1fdc6a1d64cd1c4d9135bf52177f8b171
cauams <cauanzitobnu@gmail.com>
2022-05-23 20:30:24 -0300

    Uni6Exe01 done

--- AtividadesFurb/src/unidade06/exerciciosSala/vetoresDefinidosNoCodigo/Uni6Exe01.java
+++ AtividadesFurb/src/unidade06/exerciciosSala/vetoresDefinidosNoCodigo/Uni6Exe01.java
@@ -7,14 +7,26 @@ public class Uni6Exe01 {
 
         int array[] = new int[10];
 
+        ler(array);
+        escrever(array);
+
 
     }
 
-    public int ler(int array[]) {
+    public static void ler(int array[]) {
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0)
+        for (int i = 0; i < array.length; i ++) {
+            System.out.println("Digite o número a ser inserido no vetor = " + i );
+            array[i] = scanner.nextInt();
+        }
+    }
 
+    public static void escrever(int array[]) {
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(array[i - 1]);
+        }
     }
 }
