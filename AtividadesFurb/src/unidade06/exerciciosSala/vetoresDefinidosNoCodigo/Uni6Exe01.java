7f81a62c2d9c63dd092cec70b2a10e0e9fec5bd8
cauams <cauanzitobnu@gmail.com>
2022-05-23 20:05:03 -0300

    STARTING UNIT 6

---
+++ AtividadesFurb/src/unidade06/exerciciosSala/vetoresDefinidosNoCodigo/Uni6Exe01.java
@@ -0,0 +1,20 @@
+package AtividadesFurb.src.unidade06.exerciciosSala.vetoresDefinidosNoCodigo;
+
+import java.util.Scanner;
+
+public class Uni6Exe01 {
+    public static void main(String[] args) {
+
+        int array[] = new int[10];
+
+
+    }
+
+    public int ler(int array[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0)
+
+    }
+}
